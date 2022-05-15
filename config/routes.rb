7c00813ff1a70f5908174e59e319a9781efd3574
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2022-05-15 10:32:32 +0100

    style: small improvements to UI

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources :items
   resources :warehouses
-  resources :item_warehouses
+  resources :item_warehouses, only: [:new, :create, :edit, :update]
 end
